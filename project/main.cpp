df5b48574dfe1fbaca34bfcbbd3a897f17950e00
Hadrien Milano <hadrien.milano@gmail.com>
2015-05-24 23:36:18 +0200

    screen cap WiP

--- project/main.cpp
+++ project/main.cpp
@@ -64,6 +64,69 @@ GLuint _pid_bezier;
 GLuint _pid_point;
 GLuint _pid_point_selection;
 
+
+void saveBMP(int w, int h, const char* image, ostream &stream) {
+
+	unsigned char file[14] = {
+		'B', 'M', // magic
+		0, 0, 0, 0, // size in bytes
+		0, 0, // app data
+		0, 0, // app data
+		40 + 14, 0, 0, 0 // start of data offset
+	};
+	unsigned char info[40] = {
+		40, 0, 0, 0, // info hd size
+		0, 0, 0, 0, // width
+		0, 0, 0, 0, // heigth
+		1, 0, // number color planes
+		24, 0, // bits per pixel
+		0, 0, 0, 0, // compression is none
+		0, 0, 0, 0, // image bits size
+		0x13, 0x0B, 0, 0, // horz resoluition in pixel / m
+		0x13, 0x0B, 0, 0, // vert resolutions (0x03C3 = 96 dpi, 0x0B13 = 72 dpi)
+		0, 0, 0, 0, // #colors in pallete
+		0, 0, 0, 0, // #important colors
+	};
+
+	int padSize = (4 - (w * 3) % 4) % 4;
+	int sizeData = w*h * 3 + h*padSize;
+	int sizeAll = sizeData + sizeof(file) + sizeof(info);
+
+	file[2] = (unsigned char)(sizeAll);
+	file[3] = (unsigned char)(sizeAll >> 8);
+	file[4] = (unsigned char)(sizeAll >> 16);
+	file[5] = (unsigned char)(sizeAll >> 24);
+
+	info[4] = (unsigned char)(w);
+	info[5] = (unsigned char)(w >> 8);
+	info[6] = (unsigned char)(w >> 16);
+	info[7] = (unsigned char)(w >> 24);
+
+	info[8] = (unsigned char)(h);
+	info[9] = (unsigned char)(h >> 8);
+	info[10] = (unsigned char)(h >> 16);
+	info[11] = (unsigned char)(h >> 24);
+
+	info[20] = (unsigned char)(sizeData);
+	info[21] = (unsigned char)(sizeData >> 8);
+	info[22] = (unsigned char)(sizeData >> 16);
+	info[23] = (unsigned char)(sizeData >> 24);
+
+	stream.write((char*)file, sizeof(file));
+	stream.write((char*)info, sizeof(info));
+
+	unsigned char pad[3] = { 0, 0, 0 };
+
+	for (int y = 0; y<h; y++)
+	{
+		for (int x = 0; x<w; x++)
+		{
+			stream.write((const char*)(&image[4*(x + y * width)]), 3);
+		}
+		stream.write((char*)pad, padSize);
+	}
+}
+
 void initCurves() {
 	/// Compile the shaders here to avoid the duplication
 	_pid_bezier = opengp::load_shaders("shaders/bezier_vshader.glsl", "shaders/bezier_fshader.glsl");
@@ -235,7 +298,7 @@ void display(){
     opengp::update_title_fps("FrameBuffer");
     glViewport(0,0,width,height);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
+	// glEnable(GL_BLEND); // not working on my laptop
 
     ///--- Setup view-projection matrix
     float ratio = width / (float) height;
@@ -339,6 +402,15 @@ void display(){
 	sqad.draw(view, projection, cam_pos, time);
 
 
+	char *imgData = (char*)calloc(width*height*4, sizeof(char));
+	glReadPixels(0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, imgData);
+	ofstream imgFile;
+	char filename[32];
+	sprintf(filename, "../output/out_%d.bmp", current_frame);
+	imgFile.open(filename);
+	saveBMP(width, height, imgData, imgFile);
+	imgFile.close();
+	free(imgData);
 }
 
 void update() {
